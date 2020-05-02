744a032dcc5d07344ef71e28b289d983a307d46d
maks martynov <rodya.next@mail.ru>
2020-05-02 18:38:52 +1000

    клавиатура всплывает как только открывается addItemViewController, кнопка Done (как на клавиатуре так и в панели навигации)не используется пока нет текста в поле для печати

--- CheckList/AddItemViewController.swift
+++ CheckList/AddItemViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    @IBOutlet weak var textField: UITextField!
     @IBAction func cancel() {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
         dismiss(animated: true, completion: nil)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        if newText.length > 0 {
+            doneBarButton.isEnabled = true
+        } else {
+            doneBarButton.isEnabled = false
+        }
+        return true
     }
 }
 
